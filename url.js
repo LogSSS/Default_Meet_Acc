eccf4af747a1419f447665688bc722bc4467ae27
Nazarko <106309210+LogSSS@users.noreply.github.com>
2022-05-26 21:38:36 +0300

    Update url.js

--- url.js
+++ url.js
@@ -3,8 +3,6 @@ let q = parseInt(params.get("authuser"));
 
 if (q != 1) {
     var url = new URL(window.location);
-    var search_params = url.searchParams;
-    search_params.set('authuser', '1');
-    url.search = search_params.toString();
-    window.location.href = url.toString();
+    params.set('authuser', '1');
+    window.location.search = params.toString();
 }
